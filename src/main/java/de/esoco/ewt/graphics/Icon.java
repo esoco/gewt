4947b60b5cfe5520152a0a5a8123017132cb171f
Elmar Sonnenschein <eso@esoco.de>
2016-05-18 18:58:44 +0200

    FRAMEWORK-183 (Sub-task): GEWT: add support for icons to components

---
+++ src/main/java/de/esoco/ewt/graphics/Icon.java
@@ -0,0 +1,54 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'gewt' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.ewt.graphics;
+
+/********************************************************************
+ * An image implementation that represents an icon that is identified by it's
+ * name.
+ *
+ * @author eso
+ */
+public class Icon implements Image
+{
+	//~ Instance fields --------------------------------------------------------
+
+	private final String sName;
+
+	//~ Constructors -----------------------------------------------------------
+
+	/***************************************
+	 * Creates a new instance.
+	 *
+	 * @param sIconName rName
+	 */
+	public Icon(String sIconName)
+	{
+		sName = sIconName;
+	}
+
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * Returns the name of this icon.
+	 *
+	 * @return The icon name
+	 */
+	public final String getName()
+	{
+		return sName;
+	}
+}

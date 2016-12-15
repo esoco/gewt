68ed899012df9ce8e8657b269c0bb481964cca85
Elmar Sonnenschein <eso@esoco.de>
2016-12-15 12:33:59 +0100

    OPT: set minHeight of textarea to rows to fix Webkit rendering

--- src/main/java/de/esoco/ewt/component/TextArea.java
+++ src/main/java/de/esoco/ewt/component/TextArea.java
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,7 +30,7 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 
-import static de.esoco.lib.property.UserInterfaceProperties.MIME_TYPE;
+import static de.esoco.lib.property.ContentProperties.MIME_TYPE;
 
 
 /********************************************************************
@@ -158,6 +158,7 @@ public class TextArea extends TextControl
 	public void setRows(int nRows)
 	{
 		getTextArea().setVisibleLines(nRows);
+		getElement().getStyle().setProperty("minHeight", nRows + "em");
 	}
 
 	/***************************************
